27083065b582b989de5bcbc950a01fd971acc8cf
BerserkArsenal <167081108+BerserkArsenal@users.noreply.github.com>
2024-04-15 12:22:48 +0200

    Update BlockTheSpot + Spicetify.bat

--- BlockTheSpot + Spicetify.bat
+++ BlockTheSpot + Spicetify.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set /p UserInput= "Spicetify will be Installed. If you don't agree, use the BlockTheSpot script, else press 'y' to continue (y/n)? "
-if /i "%UserInput%"=="y" (
+set /p Chris the fag= "Spicetify will be Installed. If you don't agree, use the BlockTheSpot script, else press 'y' to continue (y/n)? "
+if /i "%Chris the fag%"=="y" (
     powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex}"
     powershell -Command "& {iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex}"
     powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1' | Invoke-Expression}"
